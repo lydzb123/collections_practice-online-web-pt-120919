ebeed3ef10f0afd5f4b9b11151dde568002e463e
Lydia B <lydiabaik@gmail.com>
2019-12-19 07:43:41 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -35,7 +35,7 @@ def sum_array(num)
 end
 
 def add_s(arr)
-  arr.each_with_index.collect{|elem, i| if i != 1 do elem << "s" end
+  arr.each_with_index.collect{|elem, i| if i != 1 return elem << "s" 
 end
 
 
