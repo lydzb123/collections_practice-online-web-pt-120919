d2803d8b93700389b4fd85743a4ed037e497d1ee
Lydia B <lydiabaik@gmail.com>
2019-12-19 04:51:59 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -7,7 +7,7 @@ def sort_array_desc(arr)
 end
 
 def sort_array_char_count(arr)
-  arr.sort_by {|i| i.sort}
+  arr.sort_by {|i| i.length.sort}
 end
 
 print sort_array_char_count(["aaa", "aa", "aaaaaaa"])
