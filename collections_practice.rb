59fffdf81ded5a176d0bcd62265695531b787689
Lydia B <lydiabaik@gmail.com>
2019-12-19 07:43:58 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -35,7 +35,7 @@ def sum_array(num)
 end
 
 def add_s(arr)
-  arr.each_with_index.collect{|elem, i| if i != 1  elem << "s" }
+  arr.each_with_index.collect{|elem, i| if i != 1 then elem << "s" }
 end
 
 
